8f7638d7562eaf99bde761346a5d6c10caa1eaca
Marek <wojnarmarek0@gmail.com>
2024-12-08 05:05:31 +0100

    initial data seed

--- DataModels/ApplicationDbContext.cs
+++ DataModels/ApplicationDbContext.cs
@@ -21,6 +21,16 @@ namespace DataModels
                 .HasMany(t => t.PupilsList)
                 .WithOne(p => p.Trainer)
                 .HasForeignKey(p => p.TrainerId);
+
+            modelBuilder.Entity<Trainers>().HasData(
+             new Trainers { Id = 1, Name = "John Doe", Email = "john.doe@example.com", Password = "password123" },
+             new Trainers { Id = 2, Name = "Jane Smith", Email = "jane.smith@example.com", Password = "password456" }
+            );
+
+            modelBuilder.Entity<Pupils>().HasData(
+              new Pupils { Id = 1, Name = "Mike Johnson", Email = "mike.johnson@example.com", Password = "password789", TrainerId = 1 },
+              new Pupils { Id = 2, Name = "Emily Davis", Email = "emily.davis@example.com", Password = "password101", TrainerId = 2 }
+          );
         }
     }
 }
---
+++ DataModels/Migrations/20241208040132_DatabaseSeed.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DataModels.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Trainers",
+                columns: new[] { "Id", "Email", "Name", "Password" },
+                values: new object[,]
+                {
+                    { 1, "john.doe@example.com", "John Doe", "password123" },
+                    { 2, "jane.smith@example.com", "Jane Smith", "password456" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Pupils",
+                columns: new[] { "Id", "Email", "Name", "Password", "TrainerId" },
+                values: new object[,]
+                {
+                    { 1, "mike.johnson@example.com", "Mike Johnson", "password789", 1 },
+                    { 2, "emily.davis@example.com", "Emily Davis", "password101", 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Pupils",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Pupils",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Trainers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Trainers",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
