64a074e0e7ec5fb3e967336c3abda427501671e1
Marek <wojnarmarek0@gmail.com>
2024-12-08 06:03:23 +0100

    up

---
+++ MicroserviceRestApi/Controllers/TrainersController.cs
@@ -0,0 +1,45 @@
+﻿
+using DataModels;
+using DataModels.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MicroserviceRestApi.Controllers
+{
+    [ApiController]
+    [Route("/trainers")]
+    //[Route("/api")]
+    public class TrainersController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public TrainersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: /Trainers/5 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Trainers>> GetTrainer(int id)
+        {
+            // Pobierz trenera o podanym ID  
+            var trainer = await _context.Trainers
+                .Include(t => t.PupilsList) // Dołącz uczniów przypisanych do trenera
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            // Jeśli nie znaleziono trenera, zwróć status 404
+            if (trainer == null)
+            {
+                return NotFound();
+            }
+
+            // Zwróć dane trenera
+            return Ok(trainer);
+        }
+
+
+    }
+}
