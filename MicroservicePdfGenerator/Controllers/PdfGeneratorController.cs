2819b218dd80a6a0584d6ad0071be00064c3aff8
Marek <wojnarmarek0@gmail.com>
2025-02-06 22:02:03 +0100

    all trainers pdf generator

--- MicroservicePdfGenerator/Controllers/PdfGeneratorController.cs
+++ MicroservicePdfGenerator/Controllers/PdfGeneratorController.cs
@@ -1,6 +1,8 @@
 ﻿using PdfSharpCore.Pdf;
 using PdfSharpCore.Drawing;
 using Microsoft.AspNetCore.Mvc;
+using MicroservicePdfGenerator.Data;
+using System.Linq;
 using System.IO;
 
 namespace MicroservicePdfGenerator.Controllers
@@ -9,11 +11,19 @@ namespace MicroservicePdfGenerator.Controllers
     [Route("api/[controller]")]
     public class PdfController : ControllerBase
     {
-        [HttpGet("generate-pdf")]
+        private readonly AppDbContext _context;
+
+        public PdfController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("all-trainers")]
         public IActionResult GeneratePdf()
         {
-            string message = "Ćwiczenia zostały zapisane!";
-            string fontPath = "/usr/share/fonts/truetype/roboto/Roboto_Condensed-Light.ttf";
+            var trainers = _context.Trainers.ToList();
+
+            string message = "Lista Trenerów";
 
             using (var memoryStream = new MemoryStream())
             {
@@ -21,13 +31,32 @@ namespace MicroservicePdfGenerator.Controllers
                 var page = document.AddPage();
                 var gfx = XGraphics.FromPdfPage(page);
 
-                var font = new XFont(fontPath, 12, XFontStyle.Regular);
+                var font = new XFont("Roboto Condensed", 12);
 
                 gfx.DrawString(message, font, XBrushes.Black, new XPoint(40, 40));
 
+                double yPoint = 80;
+                var headerFont = new XFont("Roboto Condensed", 12, XFontStyle.Bold);
+                var tableFont = new XFont("Roboto Condensed", 10);
+
+                gfx.DrawString("ID", headerFont, XBrushes.Black, new XPoint(40, yPoint));
+                gfx.DrawString("Name", headerFont, XBrushes.Black, new XPoint(100, yPoint));
+                gfx.DrawString("Email", headerFont, XBrushes.Black, new XPoint(250, yPoint));
+                gfx.DrawString("Password", headerFont, XBrushes.Black, new XPoint(400, yPoint));
+                yPoint += 20;
+
+                foreach (var trainer in trainers)
+                {
+                    gfx.DrawString(trainer.Id.ToString(), tableFont, XBrushes.Black, new XPoint(40, yPoint));
+                    gfx.DrawString(trainer.Name, tableFont, XBrushes.Black, new XPoint(100, yPoint));
+                    gfx.DrawString(trainer.Email, tableFont, XBrushes.Black, new XPoint(250, yPoint));
+                    gfx.DrawString(trainer.Password, tableFont, XBrushes.Black, new XPoint(400, yPoint));
+                    yPoint += 20;
+                }
+
                 document.Save(memoryStream, false);
 
-                return File(memoryStream.ToArray(), "application/pdf", "generated-file.pdf");
+                return File(memoryStream.ToArray(), "application/pdf", "trainers-table.pdf");
             }
         }
     }
